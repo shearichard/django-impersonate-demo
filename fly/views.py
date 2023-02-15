e844e3dfb64a31df398c04e4367ce37e3593a86a
shearichard <rshea@thecubagroup.com>
2023-02-15 22:55:27 +1300

    Transition to templates

--- fly/views.py
+++ fly/views.py
@@ -4,5 +4,7 @@ from django.http import HttpResponse
 
 
 def homePageView(request):
-        return HttpResponse("Hello, Fly!")
+    context = {'dummy_list': []}
+    return render(request, 'fly/home.html', context)
+    #return HttpResponse("Hello, Fly!")
 
